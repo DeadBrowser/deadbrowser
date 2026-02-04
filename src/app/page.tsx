b4e89add41d045b9b8eaeea19b457984efbe9220
DeadBrowser <davidek6mbqf@gmail.com>
2026-02-04 21:50:12 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17931645501"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17931645501');
+</script>
+  
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
